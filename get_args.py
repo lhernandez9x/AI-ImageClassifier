295c6d5284e4545f9f88cb51fbd1bc6e8b28f1d8
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2021-07-05 17:51:15 -0600

    Update: Added type to args

--- get_args.py
+++ get_args.py
@@ -8,10 +8,10 @@ def get_input_args():
 
     #Add arguments for image directory, model,
     parser.add_argument('--arch', type=str, default='resnet152', help='Set the CNN model')
-    parser.add_argument('--GPU', default=False, help='Sets device to GPU or CPU')
-    parser.add_argument('--topk', default=5, help='Returns number of predictions')
-    parser.add_argument('--epochs', default=10, help='Set number of epochs to train the network')
-    parser.add_argument('--learning_rate', default=.001, help='Set learning rate for model')
+    parser.add_argument('--GPU', type=bool, default=False, help='Sets device to GPU or CPU')
+    parser.add_argument('--topk', type=int, default=5, help='Returns number of predictions')
+    parser.add_argument('--epochs', type=int, default=10, help='Set number of epochs to train the network')
+    parser.add_argument('--learning_rate', type=float, default=.001, help='Set learning rate for model')
     parser.add_argument('--hidden_layers', type=int, default=1024, help='Set number of hidden layers')
 
     return parser.parse_args()
